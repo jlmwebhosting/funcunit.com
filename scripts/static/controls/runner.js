843c9095631ba019e141e84f7b5fe5f650445def
Alexis Abril <dev@alexisabril.com>
2013-09-03 16:34:50 -0500

    adding cname. fixing example styles. adjusting how example is run

--- scripts/static/controls/runner.js
+++ scripts/static/controls/runner.js
@@ -4,8 +4,15 @@ can.Control('Bitovi.OSS.Runner', {
 	'click': function(el, ev) {
 		ev.preventDefault();
 
-		//starting QUnit, we'll need to refactor this when we have tabs for other frameworks
-		this.options.frame.contentWindow.QUnit.start();
+		if(!this.started) {
+			//starting QUnit, we'll need to refactor this when we have tabs for other frameworks
+			this.options.frame.contentWindow.QUnit.start();
+			this.started = true;
+		}
+		else {
+			//clears the ?init flag which is used to stop the tests from running only on initial page load
+			this.options.frame.contentWindow.location = this.options.frame.contentWindow.location.pathname;
+		}
 	}
 
 });
